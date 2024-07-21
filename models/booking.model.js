315f0f814f839eb9d4b3003bd5e7539d1b8a92db
cristinascg94 <cristinascg94@gmail.com>
2024-07-21 10:45:18 +0200

    changed boocking model

--- models/booking.model.js
+++ models/booking.model.js
@@ -6,10 +6,17 @@ const bookingSchema = new mongoose.Schema({
         ref: 'User',
         required: true
     },
-    vehicle: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Vehicle',
+    type: {
+        type: String,
         required: true
+    },
+    film: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Film',
+    },
+    serie: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Serie',
     },
     startDate: {
         type: Date,
@@ -23,22 +30,7 @@ const bookingSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
-    discount: {
-        type: Number,
-        default: 0,
-        validate: {
-            validator: function(value) {
-                return value >= 0;
-            },
-            message: 'El descuento no puede ser negativo.'
-        }
-    }
 });
-
-// Método para calcular el precio final después del descuento
-bookingSchema.methods.calculateFinalPrice = function() {
-    return this.price - this.discount;
-};
 
 const Booking = mongoose.model('Booking', bookingSchema);
 
